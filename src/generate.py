8199a71a4c02ab1fcf66946a4a97c63fe958fb04
Philipp Bergsmann <me@ph-bergsmann.com>
2023-10-22 16:02:16 +0200

    Replaces default logo

--- src/generate.py
+++ src/generate.py
@@ -10,7 +10,7 @@ TITLE = os.getenv("TITLE", "MY MEETUP TITLE")
 DATE = os.getenv("DATE", "1 January 1970")
 LOCATION = os.getenv("LOCATION", "Vienna, Austria")
 PHOTO = os.getenv("PHOTO", "https://content-files.shure.com/BlogPosts/basic-conference-room-design-a-webinar/images/basic-conference-room-design-a-webinar_header.png")
-LOGO = os.getenv("LOGO", "https://www.hubpng.com/files/preview/1280x765/png-transparent-amazon-logo-png-white-aws-logo-transparent-background-11679435173hgekgpiraebp1kcyyvtzwaozcfg7yhqxzr4plrxd4i0raq7lxwmusrvv978vom4pt6gyca7y1hma8tayjrsqe.png")
+LOGO = os.getenv("LOGO", "https://lavca.org/app/uploads/2019/10/aws-logo-square.png")
 OUTPUT_DIR = os.getenv("OUTPUT_DIR", "/output")
 
 # Open the base image
