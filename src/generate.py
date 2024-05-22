4c0f3049bf74e619450054c62777eb45abaaecee
Philipp Bergsmann <me@ph-bergsmann.com>
2024-05-22 14:16:32 +0200

    Fixes wrong default for output directory

--- src/generate.py
+++ src/generate.py
@@ -7,6 +7,6 @@ LOCATION = os.getenv("LOCATION", "Vienna, Austria")
 PHOTO = os.getenv("PHOTO", "https://content-files.shure.com/BlogPosts/basic-conference-room-design-a-webinar/images/basic-conference-room-design-a-webinar_header.png")
 PHOTO_OPACITY = int(os.getenv("PHOTO_OPACITY", 30))
 LOGO = os.getenv("LOGO", "https://lavca.org/app/uploads/2019/10/aws-logo-square.png")
-OUTPUT_DIR = os.getenv("OUTPUT_DIR", "./output")
+OUTPUT_DIR = os.getenv("OUTPUT_DIR", "/output")
 
 HeaderImageGenerator.generate(TITLE, DATE, LOCATION, PHOTO, PHOTO_OPACITY, LOGO, OUTPUT_DIR)
